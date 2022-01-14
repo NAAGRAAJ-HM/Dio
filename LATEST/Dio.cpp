64e0389dafc22acb74e6feafefdab6cfe37136b8
root <nagaraja.hm.0001@gmail.com>
2022-01-14 12:10:15 +0100

    Update: submodule Dio

--- LATEST/Dio.cpp
+++ LATEST/Dio.cpp
@@ -5,3 +5,27 @@ class_Dio Dio;
 FUNC(void, DIO_CODE) class_Dio::InitFunction(void){
 }
 
+FUNC(void, DIO_CODE) class_Dio::ReadChannel(void){
+}
+
+FUNC(void, DIO_CODE) class_Dio::WriteChannel(void){
+}
+
+FUNC(void, DIO_CODE) class_Dio::ReadPort(void){
+}
+
+FUNC(void, DIO_CODE) class_Dio::WritePort(void){
+}
+
+FUNC(void, DIO_CODE) class_Dio::ReadChannelGroup(void){
+}
+
+FUNC(void, DIO_CODE) class_Dio::WriteChannelGroup(void){
+}
+
+FUNC(void, DIO_CODE) class_Dio::GetVersionInfo(void){
+}
+
+FUNC(void, DIO_CODE) class_Dio::FlipChannel(void){
+}
+
--- LATEST/Dio.h
+++ LATEST/Dio.h
@@ -5,7 +5,15 @@
 
 class class_Dio{
    public:
-      FUNC(void, DIO_CODE) InitFunction(void);
+      FUNC(void, DIO_CODE) InitFunction      (void);
+      FUNC(void, DIO_CODE) ReadChannel       (void);
+      FUNC(void, DIO_CODE) WriteChannel      (void);
+      FUNC(void, DIO_CODE) ReadPort          (void);
+      FUNC(void, DIO_CODE) WritePort         (void);
+      FUNC(void, DIO_CODE) ReadChannelGroup  (void);
+      FUNC(void, DIO_CODE) WriteChannelGroup (void);
+      FUNC(void, DIO_CODE) GetVersionInfo    (void);
+      FUNC(void, DIO_CODE) FlipChannel       (void);
 };
 
 extern class_Dio Dio;
