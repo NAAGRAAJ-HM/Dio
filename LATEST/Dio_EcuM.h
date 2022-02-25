2ebe9f0e4e37a16fe01d867622b14fc0c20c6c7d
root <nagaraja.hm.0001@gmail.com>
2022-02-25 12:07:02 +0100

    Update: submodule Dio

--- LATEST/Dio_EcuM.h
+++ LATEST/Dio_EcuM.h
@@ -27,8 +27,8 @@ class interface_Dio_EcuM : public interface_EcuM_Client{
 /*****************************************************/
 /* FUNCTIONS                                         */
 /*****************************************************/
-      FUNC(void, DIO_CODE) InitFunction   (void);
-      FUNC(void, DIO_CODE) DeInitFunction (void);
+      virtual FUNC(void, DIO_CODE) InitFunction   (void) = 0;
+      virtual FUNC(void, DIO_CODE) DeInitFunction (void) = 0;
 };
 
 /*****************************************************/
