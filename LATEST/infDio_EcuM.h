7076ca3b9efd595ceddc842ba072dcc1f1b3a0c3
root <nagaraja.hm.0001@gmail.com>
2022-03-11 12:12:13 +0100

    Update: AUTOSAR basic software micro-controller abstraction module Dio

--- LATEST/infDio_EcuM.h
+++ LATEST/infDio_EcuM.h
@@ -1,15 +1,13 @@
+#pragma once
 /*****************************************************/
-/* File   : Dio.cpp                                  */
+/* File   : infDio_EcuM.h                            */
 /* Author : Naagraaj HM                              */
 /*****************************************************/
 
 /*****************************************************/
 /* #INCLUDES                                         */
 /*****************************************************/
-#include "module.h"
-#include "infDio_EcuM.h"
-#include "infDio_Dcm.h"
-#include "infDio_SchM.h"
+#include "Compiler_Cfg_Dio.h"
 
 /*****************************************************/
 /* #DEFINES                                          */
@@ -22,15 +20,6 @@
 /*****************************************************/
 /* TYPEDEFS                                          */
 /*****************************************************/
-class module_Dio:
-      public abstract_module
-{
-   public:
-      FUNC(void, DIO_CODE) InitFunction   (void);
-      FUNC(void, DIO_CODE) DeInitFunction (void);
-      FUNC(void, DIO_CODE) GetVersionInfo (void);
-      FUNC(void, DIO_CODE) MainFunction   (void);
-};
 
 /*****************************************************/
 /* CONSTS                                            */
@@ -43,48 +32,11 @@ class module_Dio:
 /*****************************************************/
 /* OBJECTS                                           */
 /*****************************************************/
-module_Dio     Dio;
-infEcuMClient* gptrinfEcuMClient_Dio = &Dio;
-infDcmClient*  gptrinfDcmClient_Dio  = &Dio;
-infSchMClient* gptrinfSchMClient_Dio = &Dio;
+extern infEcuMClient* gptrinfEcuMClient_Dio;
 
 /*****************************************************/
 /* FUNCTIONS                                         */
 /*****************************************************/
-FUNC(void, DIO_CODE) module_Dio::InitFunction(void){
-}
-
-FUNC(void, DIO_CODE) module_Dio::DeInitFunction(void){
-}
-
-FUNC(void, DIO_CODE) module_Dio::GetVersionInfo(void){
-}
-
-FUNC(void, DIO_CODE) module_Dio::MainFunction(void){
-}
-
-#include "Dio_Unused.h"
-
-FUNC(void, DIO_CODE) class_Dio_Unused::ReadChannel(void){
-}
-
-FUNC(void, DIO_CODE) class_Dio_Unused::WriteChannel(void){
-}
-
-FUNC(void, DIO_CODE) class_Dio_Unused::ReadPort(void){
-}
-
-FUNC(void, DIO_CODE) class_Dio_Unused::WritePort(void){
-}
-
-FUNC(void, DIO_CODE) class_Dio_Unused::ReadChannelGroup(void){
-}
-
-FUNC(void, DIO_CODE) class_Dio_Unused::WriteChannelGroup(void){
-}
-
-FUNC(void, DIO_CODE) class_Dio_Unused::FlipChannel(void){
-}
 
 /*****************************************************/
 /* EOF                                               */
