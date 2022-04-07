513727d01297dd24ae53af9dcb34230374580be6
NAGARAJA HM <nagaraja.hm.0001@gmail.com>
2022-04-07 09:24:39 +0000

    Update: AUTOSAR basic software micro-controller abstraction driver module Dio

--- LATEST/Dio.cpp
+++ LATEST/Dio.cpp
@@ -101,8 +101,8 @@ FUNC(void, DIO_CODE) module_Dio::InitFunction(
             lptrCfg = lptrCfgModule;
          }
          else{
-// use PBcfg_CanIf as back-up configuration
-            lptrCfg = PBcfg_CanIf;
+// use PBcfgCanIf as back-up configuration
+            lptrCfg = &PBcfgDio;
          }
       }
       IsInitDone = E_OK;
