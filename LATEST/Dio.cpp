12be414f6e85d0f87f441fc58b0c795ed57120aa
NAGARAJA HM <nagaraja.hm.0001@gmail.com>
2022-05-04 09:01:17 +0000

    Update: AUTOSAR basic software micro-controller abstraction driver module Dio

--- LATEST/Dio.cpp
+++ LATEST/Dio.cpp
@@ -113,7 +113,7 @@ FUNC(void, DIO_CODE) module_Dio::InitFunction(
             0 //TBD: IdModule
          ,  0 //TBD: IdInstance
          ,  0 //TBD: IdApi
-         ,  0 //TBD: IdError
+         ,  DIO_E_UNINIT
       );
 #endif
    }
@@ -133,7 +133,7 @@ FUNC(void, DIO_CODE) module_Dio::DeInitFunction(void){
             0 //TBD: IdModule
          ,  0 //TBD: IdInstance
          ,  0 //TBD: IdApi
-         ,  0 //TBD: IdError
+         ,  DIO_E_UNINIT
       );
 #endif
    }
@@ -152,7 +152,7 @@ FUNC(void, DIO_CODE) module_Dio::MainFunction(void){
             0 //TBD: IdModule
          ,  0 //TBD: IdInstance
          ,  0 //TBD: IdApi
-         ,  0 //TBD: IdError
+         ,  DIO_E_UNINIT
       );
 #endif
    }
