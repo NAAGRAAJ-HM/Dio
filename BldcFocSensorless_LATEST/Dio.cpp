9bc0b28a38845a033a8cc47c39dac659de6543a3
Nagaraja HM <nagaraja.hm.0001@gmail.com>
2022-08-03 17:36:55 +0200

    Update: AUTOSAR basic software micro-controller abstraction driver module for digital I/O

---
+++ BldcFocSensorless_LATEST/Dio.cpp
@@ -0,0 +1,180 @@
+/******************************************************************************/
+/* File   : Dio.cpp                                                           */
+/* Author : NAGARAJA HM (c) since 1982. All rights reserved.                  */
+/******************************************************************************/
+
+/******************************************************************************/
+/* #INCLUDES                                                                  */
+/******************************************************************************/
+#include "Module.hpp"
+#include "Dio.hpp"
+#include "infDio_Imp.hpp"
+
+/******************************************************************************/
+/* #DEFINES                                                                   */
+/******************************************************************************/
+#define DIO_AR_RELEASE_VERSION_MAJOR                                           4
+#define DIO_AR_RELEASE_VERSION_MINOR                                           3
+
+/******************************************************************************/
+/* MACROS                                                                     */
+/******************************************************************************/
+#if(DIO_AR_RELEASE_VERSION_MAJOR != STD_AR_RELEASE_VERSION_MAJOR)
+   #error "Incompatible DIO_AR_RELEASE_VERSION_MAJOR!"
+#endif
+
+#if(DIO_AR_RELEASE_VERSION_MINOR != STD_AR_RELEASE_VERSION_MINOR)
+   #error "Incompatible DIO_AR_RELEASE_VERSION_MINOR!"
+#endif
+
+/******************************************************************************/
+/* TYPEDEFS                                                                   */
+/******************************************************************************/
+
+/******************************************************************************/
+/* CONSTS                                                                     */
+/******************************************************************************/
+
+/******************************************************************************/
+/* PARAMS                                                                     */
+/******************************************************************************/
+
+/******************************************************************************/
+/* OBJECTS                                                                    */
+/******************************************************************************/
+VAR(module_Dio, DIO_VAR) Dio;
+
+/******************************************************************************/
+/* FUNCTIONS                                                                  */
+/******************************************************************************/
+FUNC(void, DIO_CODE) module_Dio::InitFunction(
+      CONSTP2CONST(ConstModule_TypeAbstract, DIO_CONST,       DIO_APPL_CONST) lptrConstModule
+   ,  CONSTP2CONST(CfgModule_TypeAbstract,   DIO_CONFIG_DATA, DIO_APPL_CONST) lptrCfgModule
+){
+#if(STD_ON == Dio_InitCheck)
+   if(
+         E_OK
+      != IsInitDone
+   ){
+#endif
+      if(
+            (NULL_PTR != lptrConstModule)
+         && (NULL_PTR != lptrCfgModule)
+      ){
+         lptrConst = (const ConstDio_Type*)lptrConstModule;
+         lptrCfg   = lptrCfgModule;
+      }
+      else{
+#if(STD_ON == Dio_DevErrorDetect)
+         Det_ReportError(
+               0 //TBD: IdModule
+            ,  0 //TBD: IdInstance
+            ,  0 //TBD: IdApi
+            ,  0 //TBD: IdError
+         );
+#endif
+      }
+#if(STD_ON == Dio_InitCheck)
+      IsInitDone = E_OK;
+   }
+   else{
+#if(STD_ON == Dio_DevErrorDetect)
+      Det_ReportError(
+            0 //TBD: IdModule
+         ,  0 //TBD: IdInstance
+         ,  0 //TBD: IdApi
+         ,  DIO_E_UNINIT
+      );
+#endif
+   }
+#endif
+}
+
+FUNC(void, DIO_CODE) module_Dio::DeInitFunction(
+   void
+){
+#if(STD_ON == Dio_InitCheck)
+   if(
+         E_OK
+      == IsInitDone
+   ){
+#endif
+#if(STD_ON == Dio_InitCheck)
+      IsInitDone = E_NOT_OK;
+   }
+   else{
+#if(STD_ON == Dio_DevErrorDetect)
+      Det_ReportError(
+            0 //TBD: IdModule
+         ,  0 //TBD: IdInstance
+         ,  0 //TBD: IdApi
+         ,  DIO_E_UNINIT
+      );
+#endif
+   }
+#endif
+}
+
+FUNC(void, DIO_CODE) module_Dio::MainFunction(
+   void
+){
+#if(STD_ON == Dio_InitCheck)
+   if(
+         E_OK
+      == IsInitDone
+   ){
+#endif
+#if(STD_ON == Dio_InitCheck)
+   }
+   else{
+#if(STD_ON == Dio_DevErrorDetect)
+      Det_ReportError(
+            0 //TBD: IdModule
+         ,  0 //TBD: IdInstance
+         ,  0 //TBD: IdApi
+         ,  DIO_E_UNINIT
+      );
+#endif
+   }
+#endif
+}
+
+FUNC(void, DIO_CODE) module_Dio::ReadChannel(
+   void
+){
+}
+
+FUNC(void, DIO_CODE) module_Dio::WriteChannel(
+   void
+){
+}
+
+FUNC(void, DIO_CODE) module_Dio::ReadPort(
+   void
+){
+}
+
+FUNC(void, DIO_CODE) module_Dio::WritePort(
+   void
+){
+}
+
+FUNC(void, DIO_CODE) module_Dio::ReadChannelGroup(
+   void
+){
+}
+
+FUNC(void, DIO_CODE) module_Dio::WriteChannelGroup(
+   void
+){
+}
+
+FUNC(void, DIO_CODE) module_Dio::FlipChannel(
+   void
+){
+}
+
+/******************************************************************************/
+/* EOF                                                                        */
+/******************************************************************************/
+
