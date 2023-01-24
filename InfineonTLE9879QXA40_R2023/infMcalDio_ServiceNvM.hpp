0ce8f8942589170e3e3a208de37a2b120cd11aae
Nagaraja HM <nagaraja.hm.0001@gmail.com>
2023-01-24 21:52:57 +0100

    Update: AUTOSAR basic software micro-controller abstraction driver module for digital I/O

---
+++ InfineonTLE9879QXA40_R2023/infMcalDio_ServiceNvM.hpp
@@ -0,0 +1,19 @@
+#pragma once
+/******************************************************************************/
+/* File   : infMcalDio_ServiceNvM.hpp                                         */
+/* Author : NAGARAJA HM (c) since 1982. All rights reserved.                  */
+/******************************************************************************/
+
+/******************************************************************************/
+/* #DEFINES                                                                   */
+/******************************************************************************/
+#define CONSTMCALDIO_AR_RELEASE_VERSION_MAJOR                                  4
+#define CONSTMCALDIO_AR_RELEASE_VERSION_MINOR                                  3
+
+#define Const_DefaultInit_ConstMcalDio                                                                           \
+   ,  {CONSTMCALDIO_AR_RELEASE_VERSION_MAJOR, CONSTMCALDIO_AR_RELEASE_VERSION_MINOR, 0x00,0xFF,0x01,'0','1','0'} \
+
+/******************************************************************************/
+/* EOF                                                                        */
+/******************************************************************************/
+
