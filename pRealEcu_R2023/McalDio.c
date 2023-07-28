ed3d99f451f9fe8c7a74da5a739a0528c8790ca9
Raajnaag Huliyapurada Mata <nagaraja.hm.0001@gmail.com>
2023-07-28 07:58:07 +0200

    Initial commit

--- pRealEcu_R2023/McalDio.c
+++ pRealEcu_R2023/McalDio.c
@@ -25,10 +25,11 @@
 /******************************************************************************/
 #include "Std_Types.h"
 
+#include "CfgMcalDio.h"
 #include "infMcalDioSwcApplEcuM.h"
 
 #include "uC_Dio.h"
-#include "CfgMcalDio.h"
+#include "CfgSwcServiceOs.h"
 
 /******************************************************************************/
 /* #DEFINES                                                                   */
@@ -55,33 +56,40 @@
 /******************************************************************************/
 #ifndef ReSim
 #else
-volatile uint16 P0;
-volatile uint16 P8;
-volatile uint16 P9;
-volatile uint16 P10;
-volatile uint16 PPR0;
-volatile uint16 PPR8;
-volatile uint16 PPR9;
-volatile uint16 PPR10;
-volatile uint16 PM0;
-volatile uint16 PM8;
-volatile uint16 PM9;
-volatile uint16 PM10;
-volatile uint16 PMC0;
-volatile uint16 PMC8;
-volatile uint16 PMC9;
-volatile uint16 PMC10;
-volatile uint16 PIBC0;
-volatile uint16 PIBC8;
-volatile uint16 PIBC9;
-volatile uint16 PIBC10;
-volatile uint16 PU0;
+volatile       uint16 P0;
+volatile       uint16 P8;
+volatile       uint16 P9;
+volatile       uint16 P10;
+volatile const uint16 PPR0;
+volatile const uint16 PPR8;
+volatile const uint16 PPR9;
+volatile const uint16 PPR10;
+volatile       uint16 PM0;
+volatile       uint16 PM8;
+volatile       uint16 PM9;
+volatile       uint16 PM10;
+volatile       uint16 PMC0;
+volatile       uint16 PMC8;
+volatile       uint16 PMC9;
+volatile       uint16 PMC10;
+volatile       uint16 PFC10;
+volatile       uint16 PFCE10;
+volatile       uint16 PFCAE10;
+volatile       uint16 PIBC0;
+volatile       uint16 PIBC8;
+volatile       uint16 PIBC9;
+volatile       uint16 PIBC10;
+volatile       uint16 PIPC10;
+volatile       uint16 PU0;
+volatile       uint16 PU10;
+volatile       uint8  FCLA0CTL3_INTPH;
 #endif
 
 /******************************************************************************/
 /* FUNCTIONS                                                                  */
 /******************************************************************************/
-void McalDio_vInitInput(Type_McalDio_eName leNamePin){
+//static void McalDio_vInitInput(Type_McalDio_eName leNamePin){
+         void McalDio_vInitInput(Type_McalDio_eName leNamePin){ //TBD: Remove
    const Type_McalDio_stPin* lptrstPin = &CfgMcalDio_castListPins[leNamePin];
          uint16              lu16Mask  = 1u << lptrstPin->PinNumber;
 
@@ -90,7 +98,8 @@ void McalDio_vInitInput(Type_McalDio_eName leNamePin){
   *lptrstPin->PMC_Reg  &= ~lu16Mask;
 }
 
-void McalDio_vInitOutput(Type_McalDio_eName leNamePin){
+//static void McalDio_vInitOutput(Type_McalDio_eName leNamePin){
+         void McalDio_vInitOutput(Type_McalDio_eName leNamePin){ //TBD: Remove
    const Type_McalDio_stPin* lptrstPin = &CfgMcalDio_castListPins[leNamePin];
          uint16              lu16Mask  = 1u << lptrstPin->PinNumber;
 
@@ -99,11 +108,59 @@ void McalDio_vInitOutput(Type_McalDio_eName leNamePin){
   *lptrstPin->PMC_Reg &= ~lu16Mask;
 }
 
+void McalDio_vInitOutputOff(Type_McalDio_eName leNamePin){
+  *CfgMcalDio_castListPins[leNamePin].P_Reg &= ~(1u << CfgMcalDio_castListPins[leNamePin].PinNumber);
+}
+
 static void McalDio_vInitPinErrTja(void){
-   PM0   |=  (1 << 2);
+   PM0   |=  (1 << 2); //TBD: Move to CfgMcalDio
    PIBC0 |=  (1 << 2);
    PMC0  &= ~(1 << 2);
    PU0   |=  (1 << 2);
+}
+
+static void McalDio_vInitPinFreqRF(void){
+   PM10   |=  (1 << 9);
+   PIBC10 |=  (1 << 9);
+   PMC10  &= ~(1 << 9);
+   PU10   |=  (1 << 9);
+}
+
+static void McalDio_vInitInterruptAta(uint8 lu8Val){
+   PFC10           |=  (1 << 11);
+   PFCE10          &= ~(1 << 11);
+   PFCAE10         &= ~(1 << 11);
+   PM10            |=  (1 << 11);
+   PMC10           |=  (1 << 11);
+   FCLA0CTL3_INTPH  =  2;
+   TBP11            =  1;
+   RFP11            =  0;
+   if(lu8Val == c_IRQ_ACTIVE){
+      MKP11 = L;
+   }
+   else{
+      MKP11 = H;
+   }
+}
+
+static void McalDio_vInitPinsRLin(void){
+   PFC10   |=  (1 << 6);
+   PFCE10  &= ~(1 << 6);
+   PFCAE10 &= ~(1 << 6);
+   PMC10   |=  (1 << 6);
+   PIPC10  |=  (1 << 6);
+   PM10    &= ~(1 << 6);
+   PFC10   |=  (1 << 7);
+   PFCE10  &= ~(1 << 7);
+   PFCAE10 &= ~(1 << 7);
+   PMC10   |=  (1 << 7);
+   PIPC10  |=  (1 << 7);
+   PM10    &= ~(1 << 7);
+   PFC10   |=  (1 << 8);
+   PFCE10  &= ~(1 << 8);
+   PFCAE10 &= ~(1 << 8);
+   PMC10   |=  (1 << 8);
+   PM10    |=  (1 << 8);
 }
 
 FUNC(void, MCALDIO_CODE) infMcalDioSwcApplEcuM_vInitFunction(void){
@@ -111,10 +168,13 @@ FUNC(void, MCALDIO_CODE) infMcalDioSwcApplEcuM_vInitFunction(void){
    McalDio_vInitOutput(GPIO_TP1);
    McalDio_vInitOutput(GPIO_TP2);
    McalDio_vInitOutput(GPIO_TP3);
+   McalDio_vInitPinFreqRF();
    McalDio_vInitOutput(GPIO_CS_ATA);
    McalDio_vInitOutput(GPIO_PWR_ATA);
    McalDio_vInitOutput(GPIO_PWR_B1ATA);
    McalDio_vInitOutput(GPIO_PWR_B2ATA);
+   McalDio_vInitInterruptAta(c_IRQ_NOT_ACTIVE);
+   McalDio_vInitPinsRLin();
 
 #if(CfgProject == cL_R2023)
    McalDio_vInitInput(GPIO_ERR_TJA);
@@ -126,6 +186,8 @@ FUNC(void, MCALDIO_CODE) infMcalDioSwcApplEcuM_vInitFunction(void){
 
    McalDio_vInitOutput(GPIO_STB_TJA);
    McalDio_vInitOutput(GPIO_EN_TJA);
+   McalDio_vInitOutputOff(GPIO_STB_TJA);
+   McalDio_vInitOutputOff(GPIO_EN_TJA);
 }
 
 /******************************************************************************/
